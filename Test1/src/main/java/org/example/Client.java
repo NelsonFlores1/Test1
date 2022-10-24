eef0cfd05b265f8648b01b0004e46c2945f4ae75
Nelson Flores <nelsonfloresfernandez123@gmail.com>
2022-10-24 17:39:49 -0400

    agragando de InteliiJ IDEA

---
+++ Test1/src/main/java/org/example/Client.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Client {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 8;
+        if (a>b){
+            a *= 2;
+            System.out.println(a);
+        }else{
+            a -= 2;
+            System.out.println(b);
+        }
+    }
+
+}
